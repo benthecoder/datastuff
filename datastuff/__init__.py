3ec2de6ab2b6068110268a4311b7979593434a50
benthecoder <benthecoder07@gmail.com>
2022-09-05 17:37:33 -0500

    code coverage 100%

--- datastuff/__init__.py
+++ datastuff/__init__.py
@@ -1,10 +1,16 @@
-__version__ = "0.1.0"
+"""
+This module contains a collection of functions I use for data stuff
+"""
 
-import pandas as pd
+__version__ = "0.1.0"
 
 
 def missing_vals(df, drop=False, threshold=70):
-
+    """
+    Returns a dataframe with the number of missing values and the
+    percentage of missing values. Also drops columns with more than a
+    certain percentage of missing values based on threshold
+    """
     missing_df = (
         df.isnull()
         .sum()
@@ -14,7 +20,7 @@ def missing_vals(df, drop=False, threshold=70):
         .sort_values("counts", ascending=False)
     )
 
-    if drop == True:
+    if drop:
         to_drop = list(missing_df.query(f"percentage > {threshold}").index)
         print(f"dropped columns {to_drop}")
         df = df.drop(to_drop, axis=1)
--- tests/test_datastuff.py
+++ tests/test_datastuff.py
@@ -30,3 +30,24 @@ def test_missing_vals():
     ).set_index(pd.Index(["missing_col", "color"]))
 
     assert_frame_equal(missing_df, missing_df_expected)
+
+
+def test_missing_vals_drop():
+    df = {
+        "id": [1, 2, 3, 4, 5],
+        "created_at": [
+            "2020-02-01",
+            "2020-02-02",
+            "2020-02-02",
+            "2020-02-02",
+            "2020-02-03",
+        ],
+        "color": ["red", np.NaN, "blue", "blue", "yellow"],
+    }
+
+    df = pd.DataFrame(df, columns=["id", "created_at", "color", "missing_col"])
+
+    missing_df = missing_vals(df, drop=True)
+    df_expected = df.drop("missing_col", axis=1)
+
+    assert_frame_equal(missing_df, df_expected)
